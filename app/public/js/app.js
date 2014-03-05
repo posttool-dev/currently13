65b69b9776480c367a8dc436e9232a5175f31f40
David Karam <david@posttool.com>
2014-03-05 13:29:02 -0800

    select or create in context

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -35,42 +35,34 @@ function init_cms() {
     ff.add_listener('browse', function (f, o) {
       var bb = new browse_browse(o.type);
       bb.add_listener('select', function (e, r) {
-        add_object(o.field, r);
+        o.field.push(r);
+        o.field.emit('change');
+        layers.pop_layer();
       });
       layers.add_layer(bb);
     });
     ff.add_listener('create', function (f, o) {
       var ff = form(o.type);
-      ff.add_listener('close', function (e, r) {
-        add_object(o.field, r);
+      var once = false;
+      ff.add_listener('save', function (e, r) {
+        if (!once && r.created)
+        {
+          o.field.push(r);
+          o.field.emit('change');
+          once = true;
+        }
       });
     });
     ff.add_listener('select', function (f, o) {
       var ff = form(o.type, o.id);
       ff.add_listener('close', function (e, r) {
-        update_object(o.field, r);
+//        update_object(o.field, r);//happens in layer.refresh
       });
     });
     layers.add_layer(ff);
     return ff;
   }
 
-  // glue the selected row back to the waiting form
-  function add_object(for_field, r) {
-    layers.pop_layer();
-    if (r.created) {
-      for_field.push(r);
-      for_field.emit('change');
-    }
-  }
-
-  function update_object(for_field, r) {
-    layers.pop_layer();
-    if (r.created) {
-      for_field.update(r);
-      for_field.emit('change');
-    }
-  }
 
   // root browser
   function browse(type) {
