db52a98dca82ca2ba201064c81fc844ef5f4dcaa
David Karam <david@posttool.com>
2014-07-01 11:00:26 -0700

    add env pattern, add -y to provision script

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-sudo su
-
 apt-get update
 
 apt-get install -y build-essential --no-install-recommends
 apt-get install -y python-software-properties
 apt-get install -y redis-server --no-install-recommends
 
-add-apt-repository ppa:chris-lea/node.js
-add-apt-repository ppa:dhor/myway
+add-apt-repository -y ppa:chris-lea/node.js
+add-apt-repository -y ppa:dhor/myway
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
@@ -18,3 +16,5 @@ apt-get update
 apt-get install -y graphicsmagick
 apt-get install -y nodejs
 apt-get install -y mongodb-10gen
+
+apt-get install -y htop
